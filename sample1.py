9dbda8c386134a3699e29a5cc8d0097f2fb41d04
root <root@STLININF0268.sandhata.local>
2025-09-11 16:03:09 +0530

    push request

---
+++ sample1.py
@@ -0,0 +1,12 @@
+# Syntax error: missing colon
+def broken_function()
+    print("This function is broken")
+
+# Runtime error: undefined variable
+def runtime_fail():
+    print(undeclared_variable)
+
+# Valid code for control
+def valid_function():
+    print("This is fine.")
+
